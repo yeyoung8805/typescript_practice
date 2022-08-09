436e021a5e99c1b91dbb4d079b3d81bb8f1fc30a
yeyoung8805 <yyk8805@naver.com>
2022-08-09 14:49:50 +0900

    call out typescript function method

--- test.ts
+++ test.ts
@@ -1,9 +1,11 @@
 let a: number; //Type annotation
 
-a = "Mark"; //Type 'string' is not assignable to type 'number'.
+// a = "Mark"; //Type 'string' is not assignable to type 'number'.
 a = 39;
 
 
 function hello(b: number) { //Type annotation to parameter b
 
 }
+hello(39);
+// hello("Mark"); //Argument of type 'string' is not assignable to parameter of type 'number'.
