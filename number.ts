4620711350ecc723f242178fecc2163ce572d6d5
yeyoung8805 <yyk8805@naver.com>
2022-08-09 15:50:09 +0900

    number in underscore form

--- number.ts
+++ number.ts
@@ -9,3 +9,5 @@ let binary: number = 0b1010; //2진수
 let octal: number = 0o744; //8진수
 
 let notANumber:number = NaN; //NaN 숫자가 아닌 형태
+
+let underscoreNum: number = 1_000_000; //1백만을 언더스코어 사용해서 나타낼 수 있다.
