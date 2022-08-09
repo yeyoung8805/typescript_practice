887a008d6cac3becf2b5f7c850e6076106fcb6e6
yeyoung8805 <yyk8805@naver.com>
2022-08-09 15:51:44 +0900

    number literal

--- number.ts
+++ number.ts
@@ -1,12 +1,12 @@
 //Primitive Type : Number
 
-let decimal: number = 6; //10진수
+let decimal: number = 6; //10진수 리터럴
 
-let hex: number = 0xf00d; //16진수
+let hex: number = 0xf00d; //16진수 리터럴
 
-let binary: number = 0b1010; //2진수
+let binary: number = 0b1010; //2진수 리터럴
 
-let octal: number = 0o744; //8진수
+let octal: number = 0o744; //8진수 리터럴
 
 let notANumber:number = NaN; //NaN 숫자가 아닌 형태
 
