5b462b0f63c8eed7e81cbcc2e3dc25af407d6726
yeyoung8805 <yyk8805@naver.com>
2022-08-09 17:40:57 +0900

    primitive type is not applicable

--- object.ts
+++ object.ts
@@ -1,6 +1,6 @@
 //object 란?
 //a type that represents the non-primitive type
-//(참고) non-primitive type : Not number, string, boolean, bigint, symbol, null, or undefined.
+//(참고) non-primitive type : Not (number, string, boolean, bigint, symbol, null, or undefined.)
 
 
 //create by object literal
@@ -10,3 +10,14 @@ const person1 = {name:'Mark', age:39};
 
 //create by Object.create()
 const person2 = Object.create({name:'Mark', age:39}); //Object는 런타임에 준비된 내장객체이다.
+
+let objt: object = {}; //가능하나, 보통 이렇게 사용하지 않는다.
+objt = {name: 'Mark'}; //object 넣음
+objt = [{name: 'Mark'}]; //array도 넣을 수 있음
+// objt = 39; //error : number
+// objt = 'Mark'; //error : string
+// objt = true; //error : boolean
+// objt = 100n; //error : bigint
+// objt = Symbol(); //error :symbol
+// objt = null; //error : null
+// objt = undefined; //error : undefined
