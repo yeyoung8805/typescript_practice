561254769d82918b18c65db5aa27a7c351076e60
yeyoung8805 <yyk8805@naver.com>
2022-08-09 14:56:08 +0900

    Javascript vs Typescript

--- test.ts
+++ test.ts
@@ -1,11 +1,16 @@
-let a: number; //Type annotation
+//Javascript vs Typescript
 
-// a = "Mark"; //Type 'string' is not assignable to type 'number'.
-a = 39;
+//Javascript
+// function add(n1, n2) {{
+//   if(typeof n1 !== 'number' || typeof n2 !== 'number') {
+//     throw new Error('Incorrect input!');
+//   }
+//   return n1 + n2;
+// }
+// const result = add(39, 28);
 
-
-function hello(b: number) { //Type annotation to parameter b
-
+//Typescript
+function add(n1: number, n2: number) {
+  return n1 + n2;
 }
-hello(39);
-// hello("Mark"); //Argument of type 'string' is not assignable to parameter of type 'number'.
+const result = add(39, 28);
