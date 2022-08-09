7d78e34fd89ee7f2ecd1d0beff2fadadbd086bf8
yeyoung8805 <yyk8805@naver.com>
2022-08-09 17:31:59 +0900

    object characteristic

---
+++ object.ts
@@ -0,0 +1,7 @@
+//create by object literal
+const person1 = {name:'Mark', age:39};
+//person1 is not "object" type.
+//person1 is "{name: string, age: number}" type.
+
+//create by Object.create()
+const person2 = Object.create({name:'Mark', age:39}); //Object는 런타임에 준비된 내장객체이다.
