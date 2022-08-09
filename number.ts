23d81c631379145f173c7a4e5318091b50716083
yeyoung8805 <yyk8805@naver.com>
2022-08-09 15:48:50 +0900

    primitive type : number

---
+++ number.ts
@@ -0,0 +1,11 @@
+//Primitive Type : Number
+
+let decimal: number = 6; //10진수
+
+let hex: number = 0xf00d; //16진수
+
+let binary: number = 0b1010; //2진수
+
+let octal: number = 0o744; //8진수
+
+let notANumber:number = NaN; //NaN 숫자가 아닌 형태
