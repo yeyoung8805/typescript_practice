a770bbadacf09cc095a1cb684f04570583b40abd
yeyoung8805 <yyk8805@naver.com>
2022-08-09 17:26:05 +0900

    typeof in null, undefined

--- null.ts
+++ null.ts
@@ -9,3 +9,11 @@ let v: void = undefined; //undefined 는 type 이기도 하지만 "값"이기도
 // let union: String = null; //String null check 가 true로 들어가있기때문에 안된다.
 let union: String | null = null; //union Type | 은 합집합의 의미
 union = "Mark";
+
+let n: null = null;
+console.log(n); //null
+console.log(typeof n); //런타임에서 typeof 이용해서 알아내면 null 이 아닌 object 라는 결과가 나온다.
+
+let u:undefined = undefined;
+console.log(u); //undefined
+console.log(typeof u); //undefined (런타임에서도 같은 결과)
