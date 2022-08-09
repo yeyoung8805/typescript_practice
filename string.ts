7b9d25a8ae9d5da6af9554d89f2237e62447c3d1
yeyoung8805 <yyk8805@naver.com>
2022-08-09 16:00:58 +0900

    Template String

--- string.ts
+++ string.ts
@@ -2,3 +2,15 @@ let myName: string = 'Mark';
 // let myName: string = "Mark"; //string 값은 ''도 가능하고, ""도 가능하다.
 
 myName = 'Anna';
+
+//Template String
+//행에 걸쳐있거나, 표현식을 넣을 수 있는 문자열
+let fullName: string = `Bob Bobbington`;
+let age: number = 38;
+
+let sentence: string = `Hello, my name is ${ fullName }.
+                        I'll be ${ age + 1 } years old next month.`;
+
+//template string을 사용하지 않을 경우
+let senetence: string = "Hello, my name is " + fullName + ".\n\n" +
+                        "I'll be " + (age+1) + " years old next month.";
