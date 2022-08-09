d2d2e5bd6e6e83e7dc1ef9af89694b55133907dd
yeyoung8805 <yyk8805@naver.com>
2022-08-09 15:45:01 +0900

    primitive type : boolean

---
+++ boolean.ts
@@ -0,0 +1,10 @@
+let isDone: boolean = false;
+
+isDone = true;
+console.log(typeof isDone);
+
+
+let isOk: boolean = true;
+// let isNotOk: boolean = new Boolean(true);  //Type 'Boolean' is not assignable to type 'boolean'.'boolean' is a primitive, but 'Boolean' is a wrapper object. Prefer using 'boolean' when possible.
+
+console.log(typeof isOk);
