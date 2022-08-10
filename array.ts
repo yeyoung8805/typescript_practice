e7dde5cba229d60281dd40ad8e4d7daf755aec13
yeyoung8805 <yyk8805@naver.com>
2022-08-10 09:16:38 +0900

    index not assignable to undefined

--- array.ts
+++ array.ts
@@ -3,3 +3,5 @@
 let x: [string, number];
 x = ["hello", 39];
 // x = [10, "Mark"]; // Error
+
+// x[3] = "world";  // Type '"world"' is not assignable to type 'undefined'. Tuple type '[string, number]' of length '2' has no element at index '3'.
