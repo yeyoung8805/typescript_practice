9ce196a091c517d967929c0cc98fbef5d4ac36cf
yeyoung8805 <yyk8805@naver.com>
2022-08-11 10:20:07 +0900

    prevent leaking of any

--- any.ts
+++ any.ts
@@ -10,10 +10,10 @@ let looselyTyped: any = {};
 const d = looselyTyped.a.b.c.d; //any로 지정된 곳 이후의 코드가 안전성을 잃을 수 있다. a,b,c,d 도 모두 any 타입이 전파되므로.
 
 function leakingAny(obj: any) {
-  const a = obj.num; //a는 any
-  const b = a + 1; //b도 any
+  const a: number = obj.num; //a는 number (a의 타입을 지정해줌으로써 누수를 막는 것이다!)
+  const b = a + 1; //b도 number
   return b;
 }
 
 const c = leakingAny({num: 0});
-c.indexOf("0"); //c도 any (any라는 전파(누수)가 계속 발생하고 있는 것이다.)
+// c.indexOf("0"); //c도 number 이기 때문에 string 값을 찾는 코드가 error 발생
