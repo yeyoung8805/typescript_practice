b634c9a51ae04c176956e66e0800ae620c30d176
yeyoung8805 <yyk8805@naver.com>
2022-08-11 10:41:35 +0900

    unknown, type guard

--- unknown.ts
+++ unknown.ts
@@ -1,6 +1,10 @@
 declare const maybe: unknown;
 
-const aNumber: number = maybe; //error : Type 'unknown' is not assignable to type 'number'.
+// const aNumber: number = maybe; //error : Type 'unknown' is not assignable to type 'number'.
+if(maybe === 123) { //maybe, type guarded as number
+  const aNumber: number = maybe;
+}
+
 
 if(maybe === true) { //maybe를 boolean으로 가둬놓는 이런 구문을 "타입 가드"라고 한다.
   const aBoolean: boolean = maybe; //maybe는 boolean true
@@ -8,5 +12,6 @@ if(maybe === true) { //maybe를 boolean으로 가둬놓는 이런 구문을 "타
 }
 
 if(typeof maybe === 'string') { //maybe를 string 으로 가둬놓는 이런 구문도 "타입가드"라고 한다.
-  maybe;
+  const aString: string = maybe;
+  // const aBoolean: boolean = maybe; //error
 }
