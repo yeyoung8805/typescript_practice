d3b74d5efb6c1e048090c53158ed7a2585e28e7e
yeyoung8805 <yyk8805@naver.com>
2022-08-11 11:21:59 +0900

    return undefined to function's void return type

--- void.ts
+++ void.ts
@@ -1,6 +1,6 @@
-function returnVoid(message: string) { //return 타입이 void로 추론된다.
+function returnVoid(message: string): void{ //return 타입이 void로 추론된다.
   console.log(message);
-  return;
+  return undefined; //return에는 void로 선언돼있던것에 undefined 할당 가능하다.
 }
 
 const r = returnVoid("There are no return.");
