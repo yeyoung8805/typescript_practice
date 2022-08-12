f04751885cc596e45a10bea2ae0a7c2538c3f15c
“yeyoung8805” <yyk8805@naver.com>
2022-08-12 13:10:14 +0900

    function in interface

---
+++ interface4.ts
@@ -0,0 +1,29 @@
+interface Person4 {
+  name: String;
+  age: number;
+  hello(): void; //hello() 를 개체에서 만들어 내면 된다.
+}
+
+const p41: Person4 = {
+  name: "Mark",
+  age: 39,
+  hello: function(): void {
+    console.log(`안녕하세요! ${this.name} 입니다.`);
+  },
+};
+
+const p42: Person4 = {
+  name: "Mark",
+  age: 39,
+  hello(): void { //p41와 달리 method 임을 나타내는 형식만 달라짐 
+    console.log(`안녕하세요! ${this.name} 입니다.`);
+  },
+};
+
+const p43: Person4 = {
+  name: "Mark",
+  age: 39,
+  hello: (): void => { //화살표 함수에서는 this 키워드 사용할 수 없다.
+    console.log(`안녕하세요! ${this.name} 입니다.`);
+  },
+};
