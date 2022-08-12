4f8774ece509c67a1aaa1fc74394bb895eee8a39
“yeyoung8805” <yyk8805@naver.com>
2022-08-12 13:33:28 +0900

    optional property age

--- interface7.ts
+++ interface7.ts
@@ -2,6 +2,6 @@ interface HelloPerson {
   (name: string, age?: number) : void;
 }
 
-const helloPerson: HelloPerson = function(name: string) {
+const helloPerson: HelloPerson = function(name: string, age?: number) {
   console.log(`안녕하세요! ${name} 입니다.`);
 }
