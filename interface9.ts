8fea302d83ed818a930a3fe0a248aa422f66f247
“yeyoung8805” <yyk8805@naver.com>
2022-08-12 15:25:12 +0900

    intersection in type alias VS interface

--- interface9.ts
+++ interface9.ts
@@ -14,3 +14,20 @@ type PersonList = string[];
 interface IPersonList {
   [index: number]: string;
 }
+
+//Intersection
+//compare "Intersection" in type alias VS interface
+interface ErrorHandling {
+  success: boolean;
+  error?: {message: string};
+}
+interface ArtistsData {
+  artists: {name: string}[];
+}
+//type alias 
+type ArtistsResponseType = ArtistsData & ErrorHandling;
+//interface
+interface IArtistsResponse extends ArtistsData, ErrorHandling {}
+
+let art: ArtistsResponseType;
+let iar:IArtistsResponse;
