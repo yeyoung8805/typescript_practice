4fab6bdf4ef7fed4b6bfcadc1a583ab0f29a434b
“yeyoung8805” <yyk8805@naver.com>
2022-08-12 13:13:24 +0900

    not available code in runtime

--- interface4.ts
+++ interface4.ts
@@ -20,10 +20,11 @@ const p42: Person4 = {
   },
 };
 
-const p43: Person4 = {
-  name: "Mark",
-  age: 39,
-  hello: (this: Person4): void => { //화살표 함수에서는 this 키워드 사용할 수 없다.
-    console.log(`안녕하세요! ${this.name} 입니다.`);
-  },
-};
+// cannot run this code in runtime!
+// const p43: Person4 = {
+//   name: "Mark",
+//   age: 39,
+//   hello: (this: Person4): void => { //화살표 함수에서는 this 키워드 사용할 수 없다.
+//     console.log(`안녕하세요! ${this.name} 입니다.`);
+//   },
+// };
