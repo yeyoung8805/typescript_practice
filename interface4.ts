ba5c301b0c7d2d1ac650557401865edd9649f3f6
“yeyoung8805” <yyk8805@naver.com>
2022-08-12 13:12:12 +0900

    can use this keyword in function

--- interface4.ts
+++ interface4.ts
@@ -15,7 +15,7 @@ const p41: Person4 = {
 const p42: Person4 = {
   name: "Mark",
   age: 39,
-  hello(): void { //p41와 달리 method 임을 나타내는 형식만 달라짐 
+  hello(this: Person4): void { //p41와 달리 method 임을 나타내는 형식만 달라짐 //화살표 함수가 아니라서 this 키워드 사용가능 
     console.log(`안녕하세요! ${this.name} 입니다.`);
   },
 };
@@ -23,7 +23,7 @@ const p42: Person4 = {
 const p43: Person4 = {
   name: "Mark",
   age: 39,
-  hello: (): void => { //화살표 함수에서는 this 키워드 사용할 수 없다.
+  hello: (this: Person4): void => { //화살표 함수에서는 this 키워드 사용할 수 없다.
     console.log(`안녕하세요! ${this.name} 입니다.`);
   },
 };
