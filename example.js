2e67dc1ac036927757ff3d2f37161abc90e5dcf3
“yeyoung8805” <yyk8805@naver.com>
2022-08-12 17:20:31 +0900

    creating class

--- example.js
+++ example.js
@@ -1,5 +1,8 @@
 "use strict";
 class Person {
+    constructor(name) {
+        this.name = name;
+    }
 }
-const p1 = new Person();
+const p1 = new Person("Mark");
 console.log(p1);
--- example.ts
+++ example.ts
@@ -1,5 +1,10 @@
-class Person {}
+class Person {
+  name; //프로퍼티
+  constructor(name: string) { //생성자
+    this.name = name;
+  }
+}
 
-const p1 = new Person();
+const p1 = new Person("Mark");
 
 console.log(p1);
