e5aa5153f8bcfa89c44f43063fafbcb58be8daa5
“yeyoung8805” <yyk8805@naver.com>
2022-08-12 13:38:03 +0900

    correct error

--- interface7.ts
+++ interface7.ts
@@ -2,7 +2,7 @@ interface HelloPerson {
   (name: string, age?: number) : void;
 }
 
-const helloPerson: HelloPerson = function(name: string, age: number) { //error : Type 'number | undefined' is not assignable to type 'number'.
+const helloPerson: HelloPerson = function(name: string) { //error : Type 'number | undefined' is not assignable to type 'number'.
   console.log(`안녕하세요! ${name} 입니다.`);
 }
 
