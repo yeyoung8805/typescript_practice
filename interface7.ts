0af5e228efa59caee2c88647ebcf6ca3433df88d
“yeyoung8805” <yyk8805@naver.com>
2022-08-12 13:37:10 +0900

    optional property cant be assigned to number only

--- interface7.ts
+++ interface7.ts
@@ -2,6 +2,8 @@ interface HelloPerson {
   (name: string, age?: number) : void;
 }
 
-const helloPerson: HelloPerson = function(name: string, age?: number) {
+const helloPerson: HelloPerson = function(name: string, age: number) { //error : Type 'number | undefined' is not assignable to type 'number'.
   console.log(`안녕하세요! ${name} 입니다.`);
 }
+
+helloPerson('Mark', 39);
