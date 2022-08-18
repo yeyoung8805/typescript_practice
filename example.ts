db12798c9c5b665cd9464a698663ad6ac84e527b
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 13:45:25 +0900

    readonly can initialize(change) inside constructor

--- example.ts
+++ example.ts
@@ -1,7 +1,9 @@
 class Person {
   public readonly name: string = "Mark";
-  private readonly country: string = "Korea";
-  public constructor(private _name: string, public age: number){}
+  private readonly country: string;
+  public constructor(private _name: string, public age: number){
+    this.country = "Korea"; //readonly 는 property와 constructor 안에서만 초기화하여 값을 설정해 줄수 있다. 에러 안난다.
+  }
   hello() {
     this.country = "Italy"; //error : country is a readonly property (Person 클래스내의 메소드 안이더라도)
   }
