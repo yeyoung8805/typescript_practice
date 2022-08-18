636bdc0914c236cb531f8e129330930779915e6f
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 11:30:39 +0900

    age can be number or undefined

--- example.js
+++ example.js
@@ -2,9 +2,15 @@
 class Person {
     constructor(age) {
         this.name = "Mark"; //프로퍼티
-        this.age = age;
+        if (age === undefined) {
+            this.age = 20;
+        }
+        else {
+            this.age = age;
+        }
     }
 }
 const p1 = new Person(38);
+const p2 = new Person();
 console.log(p1);
 console.log(p1.age);
--- example.ts
+++ example.ts
@@ -1,12 +1,18 @@
 class Person {
   name: string = "Mark"; //프로퍼티
   age: number; //!를 제거하고 아래 constructor 에서 정의해준다.
-  constructor(age: number){
-    this.age = age;
+  constructor(age?: number){
+    if(age === undefined ){
+      this.age = 20;
+    }else {
+      this.age = age;
+    }
+    
   }
 }
 
 const p1: Person = new Person(38);
+const p2: Person = new Person();
 
 console.log(p1);
 console.log(p1.age);
