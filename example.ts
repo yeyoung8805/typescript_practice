4e5b7e1b24970eabe38b82ff0bfee0a23d13cb65
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 13:27:16 +0900

    base of getter & setter

--- example.ts
+++ example.ts
@@ -1,7 +1,7 @@
 class Person {
   public constructor(public name: string, public age: number){}
-
 }
 
 const p1: Person = new Person("Mark", 38);
-console.log(p1);
+console.log(p1.name); //get 을 하는 함수 getter
+p1.name = "Anna"; //set을 하는 함수 setter
