32d75e3041c42bee3e01a930ac025840e0b61a66
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 15:48:41 +0900

    Generics in type alias and interface

---
+++ generis4.ts
@@ -0,0 +1,15 @@
+//type alias
+type HelloFunctionGeneric1 = <T>(message: T) => T;
+
+const helloFunction1 : HelloFunctionGeneric1 = <T>(message: T): T => {
+  return message;
+};
+
+//interface
+interface HelloFunctionGeneric2 {
+  <T>(message: T): T;
+}
+
+const helloFunction2: HelloFunctionGeneric2 = <T>(message: T): T => {
+  return message;
+};
