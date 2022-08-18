503e9bb299b32690c4889b6dfc2092982eaccbec
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 13:14:09 +0900

    adding public (access modifier)npx tsc

--- example.js
+++ example.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 class Person {
     // async constructor(age?: number){ //생성자에는 async 설정할 수 없다!
     constructor(age) {
@@ -10,8 +19,12 @@ class Person {
             this.age = age;
         }
     }
+    init() {
+        return __awaiter(this, void 0, void 0, function* () { });
+    }
 }
 const p1 = new Person(38);
+await p1.init();
 const p2 = new Person();
 console.log(p1);
 console.log(p1.age);
--- example.ts
+++ example.ts
@@ -1,18 +1,20 @@
 class Person {
-  name: string = "Mark"; //프로퍼티
-  age: number; //!를 제거하고 아래 constructor 에서 정의해준다.
+  public name: string = "Mark"; //프로퍼티
+  public age: number; //!를 제거하고 아래 constructor 에서 정의해준다.
   // async constructor(age?: number){ //생성자에는 async 설정할 수 없다!
-  constructor(age?: number){
+  public constructor(age?: number){
     if(age === undefined ){
       this.age = 20;
     }else {
       this.age = age;
     }
-    
   }
+
+  public async init(){}
 }
 
 const p1: Person = new Person(38);
+await p1.init();
 const p2: Person = new Person();
 
 console.log(p1);
