7267b743a57444750344d8229a9dbe0561e317db
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 14:05:12 +0900

    using static to property, method in class

--- example.js
+++ example.js
@@ -1,11 +1,16 @@
 "use strict";
 class Person {
-    static hello() {
+    hello() {
         console.log("안녕하세요.", Person.City);
+    }
+    change() {
+        Person.City = "LA";
     }
 }
 Person.City = "Seoul";
 const p1 = new Person();
-// p1.hello(); //error : hello() 가 static method 이기 때문
-Person.hello();
-Person.City;
+p1.hello(); //error : hello() 가 static method 이기 때문
+const p2 = new Person();
+p2.hello();
+p1.change();
+p2.hello();
--- example.ts
+++ example.ts
@@ -1,13 +1,18 @@
 class Person {
 
   public static City = "Seoul";
-  public static hello() {
+  public hello() {
     console.log("안녕하세요.", Person.City);
+  }
+  public change() {
+    Person.City = "LA";
   }
 }
 
 const p1 = new Person();
-// p1.hello(); //error : hello() 가 static method 이기 때문
+p1.hello(); //error : hello() 가 static method 이기 때문
+const p2 = new Person();
+p2.hello();
 
-Person.hello();
-Person.City;
+p1.change();
+p2.hello();
