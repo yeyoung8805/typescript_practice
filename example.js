1edb2e4175d0bf4f3194062b6aa24bf3b98c9aa5
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 13:40:29 +0900

    readonly keyword in front of property name

--- example.js
+++ example.js
@@ -3,12 +3,10 @@ class Person {
     constructor(_name, age) {
         this._name = _name;
         this.age = age;
-    }
-    get name() {
-        return this._name + "Kim";
+        this.name = "Mark";
     }
 }
 const p1 = new Person("Mark", 38);
-console.log(p1.name); //get 을 하는 함수 getter
-p1.name = "Anna"; //set을 하는 함수 setter
+console.log(p1.name);
+p1.name = "Anna"; //Person 클래스 밖에서 name 프로퍼티를 호출하려고 하면 에러 나온다.
 console.log(p1.name);
--- example.ts
+++ example.ts
@@ -1,16 +1,9 @@
 class Person {
+  public readonly name: string = "Mark";
   public constructor(private _name: string, public age: number){}
-  get name() {
-    return this._name + "Kim";
-  }
-
-  set name(n: string) {
-    console.log('set');
-    this._name = n;
-  }
 }
 
 const p1: Person = new Person("Mark", 38);
-console.log(p1.name); //get 을 하는 함수 getter
-p1.name = "Anna"; //set을 하는 함수 setter
+console.log(p1.name); 
+p1.name = "Anna"; //Person 클래스 밖에서 name 프로퍼티를 호출하려고 하면 에러 나온다.
 console.log(p1.name);
