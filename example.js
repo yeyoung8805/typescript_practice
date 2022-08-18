b05600b55bf0e74abd6c9335f33c0fe15e90694b
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 11:23:38 +0900

    add constructor and define property

--- example.js
+++ example.js
@@ -1,9 +1,10 @@
 "use strict";
 class Person {
-    constructor() {
+    constructor(age) {
         this.name = "Mark"; //프로퍼티
+        this.age = age;
     }
 }
-const p1 = new Person();
+const p1 = new Person(38);
 console.log(p1);
 console.log(p1.age);
--- example.ts
+++ example.ts
@@ -1,11 +1,12 @@
 class Person {
   name: string = "Mark"; //프로퍼티
-  age!: number; //!를 붙임으로써 클래스 내부가 아닌 곳에서 number 값을 할당해주겠다는 뜻 
-  constructor(age: number){}
+  age: number; //!를 제거하고 아래 constructor 에서 정의해준다.
+  constructor(age: number){
+    this.age = age;
+  }
 }
 
 const p1: Person = new Person(38);
 
 console.log(p1);
-// p1.age = 39;
 console.log(p1.age);
