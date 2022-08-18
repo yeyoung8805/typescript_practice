fc156afdfe9d696a23443d1462c74b09586c7aa8
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 11:21:29 +0900

    add constructor

--- example.ts
+++ example.ts
@@ -1,10 +1,11 @@
 class Person {
   name: string = "Mark"; //프로퍼티
   age!: number; //!를 붙임으로써 클래스 내부가 아닌 곳에서 number 값을 할당해주겠다는 뜻 
+  constructor(age: number){}
 }
 
-const p1 = new Person();
+const p1: Person = new Person(38);
 
 console.log(p1);
-p1.age = 39;
+// p1.age = 39;
 console.log(p1.age);
