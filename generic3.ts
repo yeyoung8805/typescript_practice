295215abd6a096bcf85dabae21404685aecbcd72
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 15:43:52 +0900

    Generics Array and Tuple (compare)

---
+++ generic3.ts
@@ -0,0 +1,18 @@
+//Array 와 Tuple의 차이점을 비교하자!!
+
+function helloArray<T>(message: T[]): T {
+  return message[0];
+}
+
+helloArray(['Hello', 'World']);
+helloArray(['Hello', 5]);
+// helloArray(['Hello', 5]).length; //error : return type이 string | number 둘 중에 어느 것인지 미리 예측하지 못한다.
+
+
+function helloTuple<T, K>(message: [T, K]): T {
+  return message[0];
+}
+
+helloTuple(['Hello', 'World']);
+helloTuple(['Hello', 5]);
+helloTuple(['Hello', 5]).length; //Tuple 타입이기 때문에 message[0]값은 string 인지 알기 때문에 내장함수 length() 사용 가능하다.
