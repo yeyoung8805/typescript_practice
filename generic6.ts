f55ac60663e530910cb3e40205b2e0a691746853
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 16:05:45 +0900

    Generic T of union type

--- generic6.ts
+++ generic6.ts
@@ -1,7 +1,11 @@
-class PersonExtends<T> {
+class PersonExtends<T extends string | number> { //T는 (유니온 타입) string 이나 number 만 가능한 것이다.
   private _name: T;
 
   constructor(name: T) {
     this._name = name;
   }
 }
+
+new PersonExtends('Mark'); 
+new PersonExtends(30); 
+new PersonExtends(true); //error : boolean은 string도 number 도 아니므로 넣을 수 없다!
