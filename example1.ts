7c428dc323bd6b1960f5e1c4b228f31708ebf0e8
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 15:17:42 +0900

    extends AbstractPerson class

--- example1.ts
+++ example1.ts
@@ -3,3 +3,14 @@ abstract class AbstractPerson {
 
   abstract setName(name: string): void;
 }
+
+// new AbstractPerson() //완전하지 못하기때문에 이렇게 인스턴스 생성 안된다.
+
+class PersonImp extends AbstractPerson {
+
+  setName(name: string): void {
+    this._name = name;
+  }
+}
+const personImp = new PersonImp();
+personImp.setName();
