64abed2af40d4800afc34a205e6252a066abf188
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 13:32:03 +0900

    adding setter method

--- example.js
+++ example.js
@@ -9,7 +9,12 @@ class Person {
         console.log('get');
         return this._name;
     }
+    set name(n) {
+        console.log('set');
+        this._name = n;
+    }
 }
 const p1 = new Person("Mark", 38);
 console.log(p1.name); //get 을 하는 함수 getter
-// p1.name = "Anna"; //set을 하는 함수 setter
+p1.name = "Anna"; //set을 하는 함수 setter
+console.log(p1.name);
--- example.ts
+++ example.ts
@@ -5,8 +5,14 @@ class Person {
     console.log('get');
     return this._name;
   }
+
+  set name(n: string) {
+    console.log('set');
+    this._name = n;
+  }
 }
 
 const p1: Person = new Person("Mark", 38);
 console.log(p1.name); //get 을 하는 함수 getter
-// p1.name = "Anna"; //set을 하는 함수 setter
+p1.name = "Anna"; //set을 하는 함수 setter
+console.log(p1.name);
