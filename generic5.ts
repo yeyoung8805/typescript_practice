af5109099574ece84d66aaf335a9b1a91d55fb4f
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 15:56:38 +0900

    more than two Generics param in class available

--- generic5.ts
+++ generic5.ts
@@ -1,9 +1,13 @@
-class PersonGeneric<T> {
+class PersonGeneric<T, K> {
   private _name: T;
-  constructor(name: T) {
+  private _age: K;
+
+  constructor(name: T, age: K) {
     this._name = name;
+    this._age = age;
   }
 }
 
-new PersonGeneric("Mark");
-new PersonGeneric<string>(30); //errpr : string 이 올 자리에 number 가 들어가 있기 때문
+new PersonGeneric("Mark", 30);
+// new PersonGeneric<string>(30); //error : string 이 올 자리에 number 가 들어가 있기 때문
+new Person<string, number>("Mark", "Anna"); //error : number 가 올자리에 string 이 들어가 있기 때문 
