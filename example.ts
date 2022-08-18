d7dddc95de68b187081b590e1684a6b7b54c55cf
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 14:15:08 +0900

    private keyword not creating instance

--- example.ts
+++ example.ts
@@ -1,7 +1,8 @@
 //class 로부터 단 하나의 object 만 생성하는 것을 싱글톤이라고 한다.
 class ClassName {
-
+  private constructor() {}
 }
 
-const a1 = new ClassName();
-const b1 = new ClassName();
+//private 을 사용했기 때문에 new 클래스명() 사용할 수 없다 .에러난다.
+// const a1 = new ClassName();
+// const b1 = new ClassName();
