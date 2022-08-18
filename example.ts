39dd76d6ca13834b8c8bca897e2984e8d12d83f2
“yeyoung8805” <yyk8805@naver.com>
2022-08-18 13:21:21 +0900

    base of initialization

--- example.ts
+++ example.ts
@@ -1,18 +1,14 @@
 class Person {
-  public name: string = "Mark"; //프로퍼티
-  private _age!: number; //!를 제거하고 아래 constructor 에서 정의해준다.
+  name: string; //프로퍼티
+  age: number; //!를 제거하고 아래 constructor 에서 정의해준다.
   // async constructor(age?: number){ //생성자에는 async 설정할 수 없다!
-  public constructor(age?: number){
-    if(age === undefined ){
-      this._age = 20;
-    }else {
-      this._age = age;
-    }
+  public constructor(name: string, age: number){
+    this.name = name;
+    this.age = age;
   }
 
-  public async init(){}
 }
 
-const p1: Person = new Person(38);
+const p1: Person = new Person("Mark", 38);
 console.log(p1);
 // console.log(p1.age); // not approachable
